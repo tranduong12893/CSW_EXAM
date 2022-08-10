8e7c623f38e51e9eeae0d9a8ae2a6826bf39a2cb
tranduong12893 <giahuy128@gmail.com>
2022-08-10 18:47:22 +0700

    1 up

---
+++ TranVanDuong/src/main/java/com/example/tranvanduong/TranVanDuongApplication.java
@@ -0,0 +1,13 @@
+package com.example.tranvanduong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranVanDuongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranVanDuongApplication.class, args);
+	}
+
+}
---
+++ TranVanDuong/src/test/java/com/example/tranvanduong/TranVanDuongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tranvanduong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranVanDuongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
